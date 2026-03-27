b6f49f1eb0d7529f7b0ccc97a25dc307da164839
BlakeLReeves <blake.reeves19@gmail.com>
2026-03-27 11:41:10 -0500

    implement search and print out matching lines

--- src/search.rs
+++ src/search.rs
@@ -1,7 +1,31 @@
+use std::fs;
+
 use crate::Config;
 
-pub fn run(config: &Config) -> Result<(), Box<dyn std::error::Error>> {
-    println!("run");
+pub fn run(Config { file_path, pattern }: &Config) -> Result<(), Box<dyn std::error::Error>> {
+    println!("Running rg-lite...");
+    println!("pattern: {}", pattern);
+    println!("file: {}", file_path);
+
+    let file_contents = fs::read_to_string(file_path)?;
+
+    let matching_contents = search_contents(pattern, &file_contents);
+
+    if matching_contents.is_empty() {
+        println!("No matching pattern in file!");
+        return Ok(());
+    }
+
+    for content in matching_contents {
+        println!("{}", content);
+    }
 
     Ok(())
+}
+
+fn search_contents<'a>(pattern: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(pattern))
+        .collect()
 }
