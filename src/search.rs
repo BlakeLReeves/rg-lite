b176f4da3e6170ba450c0b00c030e220d85d5144
BlakeLReeves <blake.reeves19@gmail.com>
2026-03-30 18:06:55 -0500

    print file path with line number and line

--- src/search.rs
+++ src/search.rs
@@ -1,7 +1,7 @@
 use colored::Colorize;
 use regex::Regex;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use crate::{Config, build_regex};
 
@@ -14,14 +14,14 @@ pub fn run(
 ) -> Result<(), Box<dyn std::error::Error>> {
     let re = build_regex(pattern, ignore_case);
     let path = Path::new(file_path);
-    let mut matching_contents: Vec<(usize, String)> = Vec::new();
+    let mut matching_contents: Vec<(PathBuf, usize, String)> = Vec::new();
     let file_contents;
 
     if path.is_dir() {
         search_dir(path, &re, &mut matching_contents)?;
     } else {
         file_contents = fs::read_to_string(path)?;
-        collect_matches(&file_contents, &re, &mut matching_contents);
+        collect_matches(&file_contents, &re, &mut matching_contents, path);
     }
 
     if matching_contents.is_empty() {
@@ -30,22 +30,29 @@ pub fn run(
     }
 
     for content in matching_contents {
-        let (idx, line) = content;
+        let (path, idx, line) = content;
         let line_number = idx + 1;
         let formatted_line = format_line(&line, &re);
 
-        println!("{}: {}", line_number, formatted_line);
+        println!("{}:{}: {}", path.display(), line_number, formatted_line);
     }
 
     Ok(())
 }
 
-fn collect_matches(contents: &str, re: &Regex, matching_contents: &mut Vec<(usize, String)>) {
+fn collect_matches(
+    contents: &str,
+    re: &Regex,
+    matching_contents: &mut Vec<(PathBuf, usize, String)>,
+    path: &Path,
+) {
     contents
         .lines()
         .enumerate()
         .filter(|(_, line)| re.is_match(line))
-        .for_each(|(idx, line)| matching_contents.push((idx, line.to_string())));
+        .for_each(|(idx, line)| {
+            matching_contents.push((path.to_path_buf(), idx, line.to_string()))
+        });
 }
 
 fn format_line(line: &str, re: &Regex) -> String {
@@ -58,7 +65,7 @@ fn format_line(line: &str, re: &Regex) -> String {
 fn search_dir(
     path: &Path,
     re: &Regex,
-    matching_contents: &mut Vec<(usize, String)>,
+    matching_contents: &mut Vec<(PathBuf, usize, String)>,
 ) -> Result<(), Box<dyn std::error::Error>> {
     for entry in fs::read_dir(path)? {
         let entry = entry?;
@@ -67,8 +74,8 @@ fn search_dir(
         if path.is_dir() {
             search_dir(&path, &re, matching_contents)?;
         } else {
-            let file_contents = fs::read_to_string(path)?;
-            collect_matches(&file_contents, &re, matching_contents);
+            let file_contents = fs::read_to_string(&path)?;
+            collect_matches(&file_contents, &re, matching_contents, &path);
         }
     }
 
